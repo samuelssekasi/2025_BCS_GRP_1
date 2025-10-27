d9658d37c1921874bbee5d6fa655bd1d6f9bf94b
CEDRIC04 <cedricahabwe04@gmail.com>
2025-10-27 11:02:06 +0300

    Add files via upload

--- vending.h
+++ vending.h
@@ -3,7 +3,9 @@ void displayMenu(struct menu items[],int size);//FUNCTION TO DISPLAY THE MENU
 int getChoice(void);//FUNCTION TO ALLOW CHOICE SELECTION
 int getPrice(struct menu items[],int choice);// FUNCTION THAT RETURNS PRICE
 void calculateChange(int change);//FUNCTION TO CALCULATE AND DISPLAY THE CHANGE
-int stockCheck(struct menu items[],int choice);//FUNCTION TO CHECK THE STOCK AND CALCULATE THE COST
+int stockCheck(struct menu items[],int choice,int size);//FUNCTION TO CHECK THE STOCK AND CALCULATE THE COST
 void restockItems(struct menu items[],int size);
 void saveStock(struct menu items[],int size);
 void loadStock(struct menu items[],int size);
+int pinVerfication(void);
+void updatePrice(struct menu items[],int size);
