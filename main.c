9e5863b278f4c3d1b16a4266ff267c36bb01b548
CEDRIC04 <cedricahabwe04@gmail.com>
2025-10-27 11:00:51 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,32 +2,35 @@
 #include"VENDING.h"
 int main(){
     int choice,quantity,money,inserted_money=0,total=0,change,price;
-    char more;
-    char mode;
+    char more,mode;
     struct menu items[]={{"Soda",1000,30},{"Water",1000,40},{"Yogurt",4000,15},{"Snacks",3000,30},{"Icecream",2000,25}};
     int size = sizeof(items)/sizeof(items[0]);
-    loadStock(items,size);
+    loadStock(items,size);//Loads and reads the stock values saved in the stock.txt
     printf("Enter mode:(C)ustomer or (O)wner:");
     scanf(" %c",&mode);
-    if(mode=='O'||mode=='o'){
+if(mode=='o'||mode=='O'){
+        if(pinVerfication()){
         restockItems(items,size);
-        printf("Updated stock:\n");
+        printf("\t\tCurrent stock\n");
         displayMenu(items,size);
+        updatePrice(items,size);
         return 0;
+}else{
+    return 0;
+}
 }
 displayMenu(items,size);
 do{
     choice =getChoice();
     price =getPrice(items,choice);
-    int cost=stockCheck(items,choice);
+    int cost=stockCheck(items,choice,size);
     total+=cost;
     printf("Your current total is UGX %d\n",total);
     printf("Do you want to purchase another item?(y/n):");
     scanf(" %c",&more);
    } while(more=='Y'||more=='y');
     printf("Final Total = UGX %d\n",total);
-    //Accept money until its atleast enough
-    while(money<total){
+    while(money<total){ //Accept money until its atleast enough
     printf("Please insert your money(UGX):");
     scanf("%d",&inserted_money);
     money+= inserted_money;
@@ -35,8 +38,7 @@ do{
     printf("Insufficient funds!!You need UGX %d more.\n",total-money);
     }
     }
-    //Change calculation
-    change = money-total;
+    change = money-total; //Change calculation
     printf("Payment accepted.Transaction being processed.\n");
     printf("Your change = UGX %d\n",change);
     if(change>0){
